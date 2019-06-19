66d1d50dfb8149535298fb938a0801813fb2e2d7
wira07dharma <48935247+wira07dharma@users.noreply.github.com>
2019-06-19 13:47:52 +0800

    Update blob.php

--- blob.php
+++ blob.php
@@ -1,168 +1,96 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once "./random_string.php";
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=azuremyteststorage;AccountKey=BvcKiWLwo3eQL8NxP3HLZ4kBQ1IJzZqcCQb9pk0lhEvxMIq4cMV6lU+Q1z+KOWjJIVmD5bxEXaqS20g9q6HfHQ==;EndpointSuffix=core.windows.net";
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+$containerName = "submission";
+	
+if (isset($_POST['submit'])) {
+	$fileToUpload = $_FILES["fileToUpload"]["name"];
+	$content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
+	echo fread($content, filesize($fileToUpload));
+		
+	$blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+	header("Location: blob.php");
+}	
+	
+$listBlobsOptions = new ListBlobsOptions();
+$listBlobsOptions->setPrefix("");
+$result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<title>Analyze With Upload Photo</title>
+		<link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+		<!-- Bootstrap core CSS -->
+		<link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+		<!-- Custom styles for this template -->
+		<link href="starter-template.css" rel="stylesheet">
+		 <style>
+       th {
+  	background-color:#707B7C; border-right:solid 1px black; border-bottom:solid 1px black; font-size:8pt ; padding:5px;font-family: arial;border-top: solid 1px black;border-left: solid 1px black;
+	} 
+	td{
+		border-right:solid 1px black; border-bottom:solid 1px black; font-size:8pt ; padding:5px;font-family: arial;border-left: solid 1px black;border-top: solid 1px black; text-align: right;  
+	}
+</style>
+	</head>
+	
+	<body>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Microsoft Azure | Dicoding</title>
-    <style>
-        body {
-            font-family: Source Code Pro, monospace;
-        }
-    </style>
-</head>
-
-<body>
-    <?php
-    /**----------------------------------------------------------------------------------
-     * Microsoft Developer & Platform Evangelism
-     *
-     * Copyright (c) Microsoft Corporation. All rights reserved.
-     *
-     * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
-     * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
-     * OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
-     *----------------------------------------------------------------------------------
-     * The example companies, organizations, products, domain names,
-     * e-mail addresses, logos, people, places, and events depicted
-     * herein are fictitious.  No association with any real company,
-     * organization, product, domain name, email address, logo, person,
-     * places, or events is intended or should be inferred.
-     *----------------------------------------------------------------------------------
-     **/
-
-    /** -------------------------------------------------------------
-# Azure Storage Blob Sample - Demonstrate how to use the Blob Storage service. 
-# Blob storage stores unstructured data such as text, binary data, documents or media files. 
-# Blobs can be accessed from anywhere in the world via HTTP or HTTPS. 
-#
-# Documentation References: 
-#  - Associated Article - https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-php 
-#  - What is a Storage Account - http://azure.microsoft.com/en-us/documentation/articles/storage-whatis-account/ 
-#  - Getting Started with Blobs - https://azure.microsoft.com/en-us/documentation/articles/storage-php-how-to-use-blobs/
-#  - Blob Service Concepts - http://msdn.microsoft.com/en-us/library/dd179376.aspx 
-#  - Blob Service REST API - http://msdn.microsoft.com/en-us/library/dd135733.aspx 
-#  - Blob Service PHP API - https://github.com/Azure/azure-storage-php
-#  - Storage Emulator - http://azure.microsoft.com/en-us/documentation/articles/storage-use-emulator/ 
-#
-     **/
-
-    require_once 'vendor/autoload.php';
-    require_once "./random_string.php";
-
-    use MicrosoftAzure\Storage\Blob\BlobRestProxy;
-    use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
-    use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
-    use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
-    use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
-
-    $connectionString = "DefaultEndpointsProtocol=https;AccountName=submissiondua;AccountKey=0vmhOZ01yJmbfp7zrM2a/7ERoqweTJvgdZBFPRfkjATHYoZPcYLPArB4F6TbKTfltQOcWQXPD0+Iz3BErp5VMQ==;EndpointSuffix=core.windows.net";
-
-    // Create blob client.
-    $blobClient = BlobRestProxy::createBlobService($connectionString);
-
-    $fileToUpload = "larrypage.jpg";
-
-    if (!isset($_GET["Cleanup"])) {
-        // Create container options object.
-        $createContainerOptions = new CreateContainerOptions();
-
-        // Set public access policy. Possible values are
-        // PublicAccessType::CONTAINER_AND_BLOBS and PublicAccessType::BLOBS_ONLY.
-        // CONTAINER_AND_BLOBS:
-        // Specifies full public read access for container and blob data.
-        // proxys can enumerate blobs within the container via anonymous
-        // request, but cannot enumerate containers within the storage account.
-        //
-        // BLOBS_ONLY:
-        // Specifies public read access for blobs. Blob data within this
-        // container can be read via anonymous request, but container data is not
-        // available. proxys cannot enumerate blobs within the container via
-        // anonymous request.
-        // If this value is not specified in the request, container data is
-        // private to the account owner.
-        $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
-
-        // Set container metadata.
-        $createContainerOptions->addMetaData("key1", "value1");
-        $createContainerOptions->addMetaData("key2", "value2");
-
-        $containerName = "emrizkiem" . generateRandomString();
-
-        try {
-            // Create container.
-            $blobClient->createContainer($containerName, $createContainerOptions);
-
-            // Getting local file so that we can upload it to Azure
-            $myfile = fopen($fileToUpload, "r") or die("Unable to open file!");
-            fclose($myfile);
-
-            # Upload file as a block blob
-            echo "Uploading BlockBlob: " . PHP_EOL;
-            echo $fileToUpload;
-            echo "<br />";
-
-            $content = fopen($fileToUpload, "r");
-
-            //Upload blob
-            $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-
-            // List blobs.
-            $listBlobsOptions = new ListBlobsOptions();
-            $listBlobsOptions->setPrefix("Buku");
-
-            echo "These are the blobs present in the container: ";
-
-            do {
-                $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
-                foreach ($result->getBlobs() as $blob) {
-                    echo $blob->getName() . ": " . $blob->getUrl() . "<br />";
-                }
-
-                $listBlobsOptions->setContinuationToken($result->getContinuationToken());
-            } while ($result->getContinuationToken());
-            echo "<br />";
-
-            // Get blob.
-            echo "This is the content of the blob uploaded: ";
-            $blob = $blobClient->getBlob($containerName, $fileToUpload);
-            fpassthru($blob->getContentStream());
-            echo "<br />";
-        } catch (ServiceException $e) {
-            // Handle exception based on error codes and messages.
-            // Error codes and messages are here:
-            // http://msdn.microsoft.com/library/azure/dd179439.aspx
-            $code = $e->getCode();
-            $error_message = $e->getMessage();
-            echo $code . ": " . $error_message . "<br />";
-        } catch (InvalidArgumentTypeException $e) {
-            // Handle exception based on error codes and messages.
-            // Error codes and messages are here:
-            // http://msdn.microsoft.com/library/azure/dd179439.aspx
-            $code = $e->getCode();
-            $error_message = $e->getMessage();
-            echo $code . ": " . $error_message . "<br />";
-        }
-    } else {
-
-        try {
-            // Delete container.
-            echo "Deleting Container" . PHP_EOL;
-            echo $_GET["containerName"] . PHP_EOL;
-            echo "<br />";
-            $blobClient->deleteContainer($_GET["containerName"]);
-        } catch (ServiceException $e) {
-            // Handle exception based on error codes and messages.
-            // Error codes and messages are here:
-            // http://msdn.microsoft.com/library/azure/dd179439.aspx
-            $code = $e->getCode();
-            $error_message = $e->getMessage();
-            echo $code . ": " . $error_message . "<br />";
-        }
-    }
-    ?>
-
-</body>
-
-</html>
+			Image to analyze:
+					<form action="blob.php" method="post" enctype="multipart/form-data">
+						<input type="file" name="fileToUpload" accept=".jpeg,.jpg,.png" required="">
+						<input type="submit" name="submit" value="Upload">
+					</form>
+			
+				<br>
+			<table>
+			<tr>
+				<th>File Name</th>
+				<th>URL</th>
+				<th>Action</th>
+			</tr>
+		
+			<tbody>
+						<?php
+						do {
+							foreach ($result->getBlobs() as $blob) {
+						?>						
+						<tr>
+							<td><?php echo $blob->getName() ?></td>
+							<td><?php echo $blob->getUrl() ?></td>
+							<td>
+								<form action="vision.php" method="post">
+									<input type="hidden" name="url" value="<?php echo $blob->getUrl()?>">						
+									<input type="submit" name="submit"  value="Lihat">
+								</form>
+							</td>
+						</tr>
+						<?php
+							} $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+						} while($result->getContinuationToken());
+						?>
+					</tbody>	
+ 				</table>
+				</div>
+			
+			<!-- Placed at the end of the document so the pages load faster -->
+			<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+			<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+			<script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
+			<script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
+			
+			</body>
+		</html>
